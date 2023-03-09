4d5bf1c0dc1641b4d1877945ae0f97fe9597cfec
idan <109270043+idanabdillah@users.noreply.github.com>
2023-03-10 05:23:58 +0700

    init eslint + eslint-config-airbnb

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    // "eslint:recommended", // replaced by airbnb codestyle
+    'plugin:react/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 0,
+  },
+};
